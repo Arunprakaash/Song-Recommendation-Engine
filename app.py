e56cfd867ddc75cab30975e725395d2ea832206b
Arunprakaash <62831206+Arunprakaash@users.noreply.github.com>
2022-06-15 20:02:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,11 @@ import numpy as np
 app = Flask(__name__)
 
 
-
 def get_cosim_artist_df(track_name, data, n):
-    print('data =',data.head())
+    print('data =', data.head())
     artist_array = np.array(data.T[track_name]).reshape(1, -1)
     dataset_array = data.drop(index=track_name).values
-    print('dataset_array',dataset_array.shape)
+    print('dataset_array', dataset_array.shape)
     cosim_scores = cosine_similarity(artist_array, dataset_array).flatten()
     track_names_array = data.drop(index=track_name).index.values
 
@@ -33,29 +32,29 @@ def get_cosim_artist_df(track_name, data, n):
     return df_result.reset_index(drop=True)
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     sp = authorize_spotify()
-    result = sp.current_user_recently_played(limit = 1)
+    result = sp.current_user_recently_played(limit=1)
     # print(result['items'][0]['track']['name'])
     return render_template('index.html')
 
-@app.route('/predict',methods = ['GET','POST'])
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         uri = []
         sp = authorize_spotify()
         data = pd.read_csv('src\data\scaled_spotify_tamil.csv')
         data.index = data['track_name']
-        data.drop_duplicates(subset = "track_name", keep="first", inplace=True)
-        data.drop('track_name',1,inplace=True)
+        data.drop_duplicates(subset="track_name", keep="first", inplace=True)
+        data.drop('track_name', 1, inplace=True)
         song = request.form['track_name']
         recommended_songs = get_cosim_artist_df(song, data, 15)
         for song in recommended_songs['Song name'].values:
             result = sp.search(q=song, type='track')
             uri.append(result['tracks']['items'][0]['uri'])
-    return render_template('index.html',prediction=recommended_songs['Song name'].values,uris = uri,num = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14])
+    return render_template('index.html', prediction=recommended_songs['Song name'].values, uris=uri, num=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
 
 
 def authorize_spotify():
@@ -65,10 +64,10 @@ def authorize_spotify():
     scope = 'user-read-private user-read-email user-library-read user-library-modify playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative'
 
     authentication = SpotifyOAuth(
-            client_id=client_id, 
-            client_secret=client_secret, 
-            redirect_uri='http://localhost:8888/callback',
-            scope=scope)
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri='http://localhost:8888/callback',
+        scope=scope)
 
     sp = spotipy.Spotify(auth_manager=authentication)
     return sp
